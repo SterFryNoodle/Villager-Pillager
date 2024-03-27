851793e4d99114ad4ec0b36a09869d19d8a96d64
SterFryNoodle <revangcruz@yahoo.com>
2024-03-26 22:17:11 -0700

    Enemy movement preset path will update after 1 second after adding coroutines

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<EnemyDestination> path = new List<EnemyDestination>(); //Initialize variable type List
+    [SerializeField] List<EnemyDestination> path = new List<EnemyDestination>(); //Initialize variable type List.
 
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(PrintWaypointName()); //How to call a coroutine similarly to calling a function.        
     }
 
-    void PrintWaypointName()
+    IEnumerator PrintWaypointName() //Return type used with foreach loops when used in coroutines.
     {
         foreach(EnemyDestination destination in path)
         {
             Debug.Log(destination.name);
+            yield return new WaitForSeconds(1f); //Will leave to Start() at this line, return to this pt after 1 second, then
+                                                 //continue the foreach loop and repeat until finish.
+
         }
     }
 }
