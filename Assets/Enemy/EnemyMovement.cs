1971b76271aa2bdb4705713ae088e6b155b8b765
SterFryNoodle <revangcruz@yahoo.com>
2024-03-26 22:41:54 -0700

    Now moves enemy object on set path created from the list in script; created variable for time delay

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -6,6 +6,8 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] List<EnemyDestination> path = new List<EnemyDestination>(); //Initialize variable type List.
 
+    float delayMoveUpdate = 1f;
+
     void Start()
     {
         StartCoroutine(PrintWaypointName()); //How to call a coroutine similarly to calling a function.        
@@ -15,10 +17,9 @@ public class EnemyMovement : MonoBehaviour
     {
         foreach(EnemyDestination destination in path)
         {
-            Debug.Log(destination.name);
-            yield return new WaitForSeconds(1f); //Will leave to Start() at this line, return to this pt after 1 second, then
+            transform.position = destination.transform.position;
+            yield return new WaitForSeconds(delayMoveUpdate); //Will leave to Start() at this line, return to this pt after 1 second, then
                                                  //continue the foreach loop and repeat until finish.
-
         }
     }
 }
