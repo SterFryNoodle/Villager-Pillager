4735a8d8a1fe90325d40dfc8a73d7b0b19162901
SterFryNoodle <revangcruz@yahoo.com>
2024-03-31 13:17:40 -0700

    Updated enemy movement to update based on intervals between frames and rotates based on direction of next destination

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] List<EnemyDestination> path = new List<EnemyDestination>(); //Initialize variable type List.
-
-    float delayMoveUpdate = 1f;
-
+    
     void Start()
     {
-        StartCoroutine(PrintWaypointName()); //How to call a coroutine similarly to calling a function.        
+        StartCoroutine(FollowPath());
     }
 
-    IEnumerator PrintWaypointName() //Return type used with foreach loops when used in coroutines.
+    IEnumerator FollowPath() //Return type used with foreach loops when used in coroutines.
     {
         foreach(EnemyDestination destination in path)
         {
-            transform.position = destination.transform.position;
-            yield return new WaitForSeconds(delayMoveUpdate); //Will leave to Start() at this line, return to this pt after 1 second, then
-                                                 //continue the foreach loop and repeat until finish.
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = destination.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition); // rotates object based on forward movement of wherever the next destination is.
+            
+            while(travelPercent <= 1f)
+            {
+                travelPercent += Time.deltaTime; // increases variable based on frame time.
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent); // sets current object position to a percentage to the way between start and end positions.
+
+                yield return new WaitForEndOfFrame();
+            }            
         }
     }
 }
