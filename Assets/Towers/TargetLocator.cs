b75abe79f39ad21845cd69eac37e85aae67bf72a
SterFryNoodle <revangcruz@yahoo.com>
2024-04-03 21:13:14 -0700

    Added a target locator script to tower prefab

---
+++ Assets/Towers/TargetLocator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Transform weapon;
+
+    void Start()
+    {
+        AimWeapon();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void AimWeapon()
+    {
+
+    }
+}
