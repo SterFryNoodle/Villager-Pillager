1cafb6c3e0db6635fcfb43d6790bc87a7d0a0bc4
SterFryNoodle <revangcruz@yahoo.com>
2024-04-07 00:24:34 -0700

    Adjusted targetLocator script to change enemy target based on their distance

--- Assets/Towers/TargetLocator.cs
+++ Assets/Towers/TargetLocator.cs
@@ -6,18 +6,32 @@ public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] Transform weapon;
-
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMovement>().transform;        
-    }
-
-    
+            
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestEnemy = null;
+        float maxDistance = Mathf.Infinity; //Sets distance and assigns to max value possible.
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position); //Assigns the distance between the position of towers and position of enemy it targets.
+
+            if (targetDistance < maxDistance)
+            {
+                closestEnemy = enemy.transform;
+                maxDistance = targetDistance; //adjusts max distance to difference of distance value of current enemy target.
+            }
+            
+        }
+        target = closestEnemy;
+    }
     void AimWeapon()
     {
         weapon.LookAt(target);
