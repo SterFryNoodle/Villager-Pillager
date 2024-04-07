5b23ef72aca94ec3bd869eac0a096b377f80ef9b
SterFryNoodle <revangcruz@yahoo.com>
2024-04-07 16:15:55 -0700

    Added an attack range for towers and disables emission of bolts if their target exits the tower range; Adjusted prefab to changes of targetLocator script

--- Assets/Towers/TargetLocator.cs
+++ Assets/Towers/TargetLocator.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float towerRange = 15f;
+    [SerializeField] ParticleSystem boltParticles;
     [SerializeField] Transform weapon;
-            
+        
     void Update()
     {
         FindClosestTarget();
@@ -21,11 +23,11 @@ public class TargetLocator : MonoBehaviour
 
         foreach (Enemy enemy in enemies)
         {
-            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position); //Assigns the distance between the position of towers and position of enemy it targets.
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position); //Checks the distance between the position of towers and position of enemy it targets.
 
             if (targetDistance < maxDistance)
             {
-                closestEnemy = enemy.transform;
+                closestEnemy = enemy.transform; //Add safeguards to find closest enemy after current target dies or goes out of range.
                 maxDistance = targetDistance; //adjusts max distance to difference of distance value of current enemy target.
             }
             
@@ -34,6 +36,23 @@ public class TargetLocator : MonoBehaviour
     }
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position); //Another distance check.
+
+        if(targetDistance > towerRange)
+        {
+            Attack(false);
+        }
+        else
+        {
+            Attack(true);
+        }
+
         weapon.LookAt(target);
+    }
+
+    void Attack(bool isActive)
+    {
+        var enableEmission = boltParticles.emission;
+        enableEmission.enabled = isActive; 
     }
 }
