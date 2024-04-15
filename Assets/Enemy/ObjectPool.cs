d447af54fea2c4f1bc38ab0b8ca655ac513c5f33
SterFryNoodle <revangcruz@yahoo.com>
2024-04-14 23:08:21 -0700

    Added soldier enemy prefab; Updated objectpool script to fill using multiple prefabs(Does not update to spawn other prefabs, need to fix)

--- Assets/Enemy/ObjectPool.cs
+++ Assets/Enemy/ObjectPool.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    [SerializeField][Range(0.1f, 15f)] float spawnTimer = 1f;
+    [SerializeField][Range(0.1f, 15f)] float spawnTimer = 2f;
     [SerializeField][Range(0, 50)] int poolSize = 5;
-    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject[] enemyPrefab;
 
     GameObject[] pool;
-
-    void Awake()
-    {
-        PopulatePool();
-    }
 
     void Start()
     {
         StartCoroutine(SpawnEnemy());
+    }
+
+    void Awake()
+    {
+        PopulatePool();        
     }
 
     IEnumerator SpawnEnemy()
@@ -25,11 +25,10 @@ public class ObjectPool : MonoBehaviour
         while (true)
         {
             EnableObjectInPool();
-
-            if (spawnTimer > 0f)
+            if (spawnTimer > 0)
             {
                 yield return new WaitForSeconds(spawnTimer);
-            }            
+            }
         }
     }
      void EnableObjectInPool()
@@ -47,11 +46,12 @@ public class ObjectPool : MonoBehaviour
     void PopulatePool()
     {
         pool = new GameObject[poolSize]; //Initializes pool array with size.
+        int randomindex = Random.Range(0, enemyPrefab.Length);
 
-        for(int i = 0; i < pool.Length; i++)
-        {
-            pool[i] = Instantiate(enemyPrefab, transform);
-            pool[i].SetActive(false); //Sets gameObjects within array as disabled by default.
+        for (int i = 0; i < pool.Length; i++)
+        {            
+            pool[i] = Instantiate(enemyPrefab[randomindex], transform);
+            pool[i].SetActive(false); //Sets gameObjects within array as disabled by default.            
         }
     }    
 }
